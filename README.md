db5236a426841005d6dd462e50aeb077c99ef700
Rudraa Khadela <rrudrakhadela@gmail.com>
2026-02-10 13:13:57 +0530

    folder

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Medical-Chatbot
+
+### conda environment
+
+```bash
+conda create -n medibot python=3.10 -y
+```
+
+```bash
+conda activate medibot
+```
+
+install reqirements
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "medical-chatbot",
+    version= "0.0.0",
+    author= "Buppy Ahmed",
+    author_email= "entbappy73@gmil.com",
+    packages=find_packages(),
+    install_requires= []
+)
